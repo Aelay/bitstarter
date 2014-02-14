08e778b1391794fedfa3f49e6bd3ab01ea2b4e17
Alexey Levin <alexej.levin@gmail.com>
2014-02-14 10:11:08 +0000

    ok, looks like i didnt git add web.js last time, so lets try that.

--- web.js
+++ web.js
@@ -2,8 +2,17 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
+var maxBuffersize = 100;
+
+var output = new String;
+var rawoutput = new Buffer(maxBuffersize);
+
+fs.readFile('index.html', rawoutput);
+output = rawoutput.toString();              
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+//  response.send('Hello World 2!');
+  response.send(output);
 });
 
 var port = process.env.PORT || 5000;
