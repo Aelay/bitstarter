ad074fd3a40a2b851d8451aef462a24884f1d5e4
Alexey Levin <alexej.levin@gmail.com>
2014-03-07 22:13:51 +0000

    In the end I figured, I don't need buffers if I specify encoding.

--- web.js
+++ web.js
@@ -3,27 +3,27 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
-var maxBuffersize = 100;
+// var maxBuffersize = 1000;
 
 var output = "";
-var rawoutput = new Buffer(maxBuffersize);
+// var rawoutput = new Buffer(maxBuffersize);
 
-
-fs.readFile('index.html', 'utf8', function (err, data) {
+output = fs.readFileSync('index.html','utf8', function (err, data)  {
+  var output = "";
   if (err) throw err;
-  rawoutput=data;
-});
+  console.log(data);
+  return data;
+  });
 
-
-len = rawoutput.length-1;
+//len = rawoutput.length-1;
 
 /* I wanted to get rid of \n at the end by reading
  * length-1 bytes, but somehow, whatever number I
  * write as last argument for .toString,
  * the result in output is always the same. */
 
-output = rawoutput.toString('utf8',0,len);
-
+//output = rawoutput.toString();
+//console.log(output);
 
 app.get('/', function(request, response) {
 //  response.send('Hello World 2!');
