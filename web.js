77da43750b9dd8ceff43290c26bd895d0c73c93b
Alexey Levin <alexej.levin@gmail.com>
2014-03-07 05:48:21 +0000

    Added encoding to buffer. Seems better now

--- web.js
+++ web.js
@@ -8,16 +8,25 @@ var maxBuffersize = 100;
 var output = "";
 var rawoutput = new Buffer(maxBuffersize);
 
-fs.readFile('index.html', function (err, data) {
+
+fs.readFile('index.html', 'utf8', function (err, data) {
   if (err) throw err;
   rawoutput=data;
 });
-output = rawoutput.toString();
+
+
+len = rawoutput.length-1;
+
+//I wanted to get rid of \n at the end by reading length-1 bytes, but somehow, whatever number I write as last argument for .toString, the result in output is always the same.
+
+output = rawoutput.toString('utf8',0,len);
+
 
 app.get('/', function(request, response) {
 //  response.send('Hello World 2!');
   response.send(output);
 });
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
