c226e574cbee6e73cb88261d956afd97ca8c6b49
Ubuntu <ubuntu@ip-172-31-16-142.eu-west-1.compute.internal>
2014-03-27 19:07:44 +0000

    deleted comments from web.js, modified port to 8080

--- web.js
+++ web.js
@@ -3,10 +3,7 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
-// var maxBuffersize = 1000;
-
 var output = "";
-// var rawoutput = new Buffer(maxBuffersize);
 
 output = fs.readFileSync('index.html','utf8', function (err, data)  {
   var output = "";
@@ -15,23 +12,12 @@ output = fs.readFileSync('index.html','utf8', function (err, data)  {
   return data;
   });
 
-//len = rawoutput.length-1;
-
-/* I wanted to get rid of \n at the end by reading
- * length-1 bytes, but somehow, whatever number I
- * write as last argument for .toString,
- * the result in output is always the same. */
-
-//output = rawoutput.toString();
-//console.log(output);
-
 app.get('/', function(request, response) {
-//  response.send('Hello World 2!');
   response.send(output);
 });
 
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
